9ca8232cd89bd6a517b1ce0fcc15558770a4d7c3
Nikola_Stanišljević <stanisljevicnikola@gmail.com>
2022-05-24 14:49:48 +0200

    End of section 6

--- API/client/src/app/nav/nav.component.ts
+++ API/client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountSkipTestsService } from '../_services/account.service';
@@ -13,7 +15,8 @@ export class NavComponent implements OnInit {
   model: any = {}
 
 
-  constructor(public accountService: AccountSkipTestsService) { }
+  constructor(public accountService: AccountSkipTestsService, private router: Router,
+     private toastr: ToastrService) { }
 
   ngOnInit(): void {
 
@@ -21,15 +24,17 @@ export class NavComponent implements OnInit {
   
   login(){
     this.accountService.login(this.model).subscribe(response =>{
-      console.log(response);
+      this.router.navigateByUrl("/members")
     },error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
     
     
   }
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl("/")
 
   }
 
--- API/client/src/app/register/register.component.ts
+++ API/client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountSkipTestsService } from '../_services/account.service';
 
 
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService : AccountSkipTestsService) { }
+  constructor(private accountService : AccountSkipTestsService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +22,7 @@ export class RegisterComponent implements OnInit {
     this.cancel();
    }, error => {
     console.log(error);
+    this.toastr.error(error.error);
    })
     
   }
